59ddc904e4a08a013a0260ed481b9adf2b9af0e5
João Barreto <joaobarretoprof@gmail.com>
2024-09-06 13:07:33 -0300

    Iniciando o Projeto API FutebolBrasileiro

---
+++ api/routes/teamRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+const teamRoutes = express.Router();
+import teamController from "../controllers/teamController.js";
+import TeamService from "../services/TeamService";
+
+//Endpoint para listar todos os times
+teamRoutes.get("/teams", teamController.getAllTeams);
+
+export default teamRoutes;
