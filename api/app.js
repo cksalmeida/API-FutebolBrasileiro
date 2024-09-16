e934a065787415a980d4b1b9808976896636defd
João Barreto <joaobarretoprof@gmail.com>
2024-09-16 20:35:44 -0300

    feat: add cors

--- api/app.js
+++ api/app.js
@@ -1,10 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import { connections } from 'mongoose';
 import mongoose from './config/db-connection.js';
-import teamRoutes from './routes/teamRoutes.js'
+import teamRoutes from './routes/teamRoutes.js';
 import playerRoutes from './routes/playerRoutes.js';
 
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:5173', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  credentials: true, 
+};
+
+app.use(cors(corsOptions)); 
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -13,7 +22,7 @@ app.get("/", (req, res) => {
   res.json({ message: "API de Futebol Brasileiro está rodando!" });
 });
 
-app.use('/api', teamRoutes)
+app.use('/api', teamRoutes);
 app.use('/api', playerRoutes);
 
 const port = 4000;
